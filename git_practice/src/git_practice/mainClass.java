1d5373403d7b30eef07450524de3702a1eea2d2d
eiyouwang <mojiey@126.com>
2018-01-04 15:42:30 +0800

    second commit

--- git_practice/src/git_practice/mainClass.java
+++ git_practice/src/git_practice/mainClass.java
@@ -4,7 +4,7 @@ public class mainClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("this is the git test!");
+		System.out.println("this is the git test master!");
 
 	}
 
